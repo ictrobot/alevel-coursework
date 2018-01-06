fb72136e834aa19c34ba951ee9453cdd0c1be581
ictrobot <ictrobot@outlook.com>
2018-01-06 15:17:52 +0000

    document show formatting 2

---
+++ test.py
@@ -0,0 +1,7 @@
+# This is a test for document formatting
+
+number1 = int(input("Please enter a number? "))
+number2 = int(input("Please enter another number? "))
+
+answer = number1 + number2
+print("The answer is {}".format(answer))
