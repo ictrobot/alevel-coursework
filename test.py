d4a399fa9712d78bf9dd263f7981021f1092d05b
ictrobot <ictrobot@outlook.com>
2018-01-06 15:18:48 +0000

    document show formatting 3

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 # This is a test for document formatting
 
+# ask the user for two numbers
 number1 = int(input("Please enter a number? "))
 number2 = int(input("Please enter another number? "))
 
+# print the sum
 answer = number1 + number2
-print("The answer is {}".format(answer))
+print("The sum is {}".format(answer))
