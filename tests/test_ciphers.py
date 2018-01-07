9825e015492d0fc0a6d45f3346ddd97f1eda79aa
ictrobot <ictrobot@outlook.com>
2018-01-07 19:59:47 +0000

    add scytale unittests

--- tests/test_ciphers.py
+++ tests/test_ciphers.py
@@ -1,6 +1,7 @@
 import unittest
 
 from ciphers.caesar import caesar
+from ciphers.scytale import reverse_scytale, scytale
 
 
 class TestCiphers(unittest.TestCase):
@@ -12,3 +13,11 @@ class TestCiphers(unittest.TestCase):
     def test_caesar_reverse(self):
         self.assertEqual(caesar("Spwwz Hzcwo", -11), "Hello World")
         self.assertEqual(caesar("Trvjri Tzgyvi zj r dfefrcgyrsvkzt jlsjkzklkzfe tzgyvi", -17), "Caesar Cipher is a monoalphabetic substitution cipher")
+
+    def test_scytale(self):
+        self.assertEqual(scytale("TranspositionCiphers", 5), "TpiproohasneniCrstis")
+        self.assertEqual(scytale("AncientGreeks", 4), "Aersnne_cte_iGk_")
+
+    def test_scytale_reverse(self):
+        self.assertEqual(reverse_scytale("TpiproohasneniCrstis", 5), "TranspositionCiphers")
+        self.assertEqual(reverse_scytale("Aersnne_cte_iGk_", 4), "AncientGreeks")
