4cf72bef3e6db2545e2542747d6c8e4104509dcd
ictrobot <ictrobot@outlook.com>
2018-01-07 16:42:34 +0000

    add application class

--- src/main.py
+++ src/main.py
@@ -5,9 +5,32 @@ from ciphers.caesar import CaesarDecrypt, CaesarEncrypt
 from utilities import SUBTITLE_LABEL_OPTIONS, TITLE_LABEL_OPTIONS
 
 
+class Application(tk.Tk):
+    """Handles swapping between windows"""
+
+    def __init__(self):
+        super(Application, self).__init__()
+        # set the window title & that it is not resizable.
+        self.title("Ciphers")
+        self.resizable(0, 0)
+        self.frame = None
+        # When first opened, display the Main Menu
+        self.show(MainMenu)
+
+    def show(self, frame_class=None, *args, **kwargs):
+        """ Constructs an instance of the class frame_class and shows the resulting frame """
+        # remove the old frame if there was one
+        if self.frame is not None:
+            self.frame.forget()
+        # make the new frame and pack it into the window
+        self.frame = frame_class(self, *args, **kwargs)
+        self.frame.pack()
+
+
 class MainMenu(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
+    def __init__(self, application):
+        super().__init__(application)
+        self.application = application
         self.create_widgets()
 
     def create_widgets(self):
@@ -18,22 +41,10 @@ class MainMenu(tk.Frame):
         tk.Label(self, text="Simple Substitution Ciphers", **SUBTITLE_LABEL_OPTIONS).grid(row=1, sticky="NW")
 
         tk.Label(self, text="Caesar Cipher").grid(row=2, sticky="NW")
-        tk.Button(self, text="Encrypt", command=lambda: self.show_cipher(CaesarEncrypt)).grid(row=2, sticky="NESW", column=1)
-        tk.Button(self, text="Decrypt", command=lambda: self.show_cipher(CaesarDecrypt)).grid(row=2, sticky="NESW", column=2)
-
-    def show_cipher(self, cipher_class):
-        """Display the cipher window"""
-        cipher_instance = cipher_class()
-        new_root = tk.Tk()
-        CipherWindow(new_root, cipher_instance).pack()
-        # destroy main menu
-        self.master.destroy()
-        # run new window
-        new_root.mainloop()
+        tk.Button(self, text="Encrypt", command=lambda: self.application.show(CipherWindow, CaesarEncrypt())).grid(row=2, sticky="NESW", column=1)
+        tk.Button(self, text="Decrypt", command=lambda: self.application.show(CipherWindow, CaesarDecrypt())).grid(row=2, sticky="NESW", column=2)
 
 
 if __name__ == "__main__":
     # Only start the program when this file is run, not when it is imported.
-    root = tk.Tk()
-    MainMenu(root).pack()
-    root.mainloop()
+    Application().mainloop()
