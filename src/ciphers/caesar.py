15d368870fe72179720dd58c175a1a02e57f7dd7
ictrobot <ictrobot@outlook.com>
2018-02-05 21:39:36 +0000

    update caesar cipher according to benchmarks

--- src/ciphers/caesar.py
+++ src/ciphers/caesar.py
@@ -3,27 +3,39 @@ import tkinter as tk
 from cipher_window import CipherWindow
 from widgets.numentry import NumEntry
 
+from string import ascii_letters
+
+
+def _caesar_chr(chr_code, shift):
+    """This function preforms a caesar shift on a single letter as a character code. Used to pre-calculate the translations"""
+    if 65 <= chr_code <= 90:
+        # upper case letters
+        offset = 65
+    elif 97 <= chr_code <= 122:
+        # lower case letters
+        offset = 97
+    else:
+        # leave other characters untouched
+        return chr_code
+    index = chr_code - offset
+    index += shift
+    return offset + (index % 26)
+
+
+# pre-calculate the translations for the caesar cipher
+CAESAR = {}
+for shift in range(26):
+    translation = {}
+    for letter in ascii_letters:
+        chr_code = ord(letter)
+        output_chr_code = _caesar_chr(chr_code, shift)
+        translation[chr_code] = output_chr_code
+    CAESAR[shift] = translation
+
 
 def caesar(text, shift):
     """This function preforms a caesar shift on the input text and returns the output. Any characters which are not letters are left unchanged."""
-    output = ""
-    for letter in text:
-        chr_code = ord(letter)
-        if 65 <= chr_code <= 90:
-            # upper case letters
-            offset = 65
-        elif 97 <= chr_code <= 122:
-            # lower case letters
-            offset = 97
-        else:
-            # leave other characters untouched
-            output += letter
-            continue
-        index = chr_code - offset
-        index += shift
-        chr_code = offset + (index % 26)
-        output += chr(chr_code)
-    return output
+    return text.translate(CAESAR[shift % 26])
 
 
 class CaesarCipher(CipherWindow):
